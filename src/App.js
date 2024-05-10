4dfc484234a9664b8a2b0d1076aa685373fc42dc
amirah <mirez.data@gmail.com>
2024-05-11 04:24:05 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/crud-react_redux'>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/create' element={<Create />}></Route>
